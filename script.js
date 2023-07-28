e4f5b2794ac84db8e9ea56bfd409e1f2b3f5739e
carolinekle <caroline.klewinowski@gmail.com>
2023-07-27 22:09:27 -0400

    get the ball rolling on js

---
+++ script.js
@@ -0,0 +1,27 @@
+const player = (name, field) => {
+    const getField = () => field;
+    const getName  = () => name;
+  };
+  
+
+  const gameBoard = (() => {
+    const board = ["", "", "", "", "", "", "", "", ""];
+    
+    const setFields = () =>{
+         const fields = document.querySelector("[data-field]")
+        fields 
+    }
+    
+    const setMarker = () => {
+    }
+  })
+
+  const getName = () => {
+    //query select from input
+    return //field
+  }
+
+  const getField = () => {
+    // query select ?
+    return //name
+  }
