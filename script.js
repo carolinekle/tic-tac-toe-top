642a6279179ed684cd57c065116fa032b977ce12
carolinekle <caroline.klewinowski@gmail.com>
2023-07-30 21:25:58 -0400

    working on js

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-const playerSign = () => {
-    const playerX = "X"
-    const playerO = "O"
-    return{playerO, playerX}
+const player = () => {
+
+    return{playerName, sign}
   };
   
 
   const gameBoard = (() => {
     const board = ["", "", "", "", "", "", "", "", ""];
     
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+      const setMarker = () => {
+        board[index] = sign
+    }
+
     const getFields = () => {
-      const fields = document.querySelectorAll("[data-field]")
-      fields.addEventListener("click", setMarker,{
-      once: true
-      })
+      return[board]
     }
-    const setMarker = () => {
-      fiends.innerText = "bye";
-    }
-  })
+    return {setMarker,getFields}
+  })();
+
+  const displays = (() => {
+    const fields = document.querySelectorAll("[data-field]")
+    fields.addEventListener("click", handleClick, {once: true})
+  })();
 
   const getName = () => {
-    const playerXName = document.querySelector('[name="playerOne"]');
-    const playerOName = document.querySelector('[name="playerTwo"]')
-    return  {playerXName, playerOName}
+    const submitNames = document.querySelector(".submit-names")
+    submitNames.addEventListener("click", () => {
+      const playerXName = document.querySelector('[name="playerOne"]').value;
+      const playerOName = document.querySelector('[name="playerTwo"]').value;
+      const playerX = player(playerXName, "✗")
+      const playerO = player(playerOName, "ꙩ")
+      return  {playerX, playerO}
+    })
   }
-
 
   const restartGame = () =>{
     const restart = document.querySelector(".restart")
@@ -32,8 +49,3 @@ const playerSign = () => {
       gameBoard;
   })
 }
-
-
-
-//✗
-//ꙩ
